478fc45aa75d394fd724a0d5274b89d895f052c1
Dinika03 <78576083+Dinika03@users.noreply.github.com>
2025-08-12 16:48:51 -0400

    Update datalayerEDDL.js

--- datalayerEDDL.js
+++ datalayerEDDL.js
@@ -1,3 +1,14 @@
 window.EDDLdataLayer = window.EDDLdataLayer || [];
 
+document.addEventListener("DOMContentLoaded", function() {
+  window.EDDLdataLayer.push({
+    event: "pageview",
+    page: {
+      pageName: document.title,
+      url: window.location.href,
+    }
+   
+  });
+});
+
 
