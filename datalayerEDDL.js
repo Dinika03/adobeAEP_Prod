9aa2deee7893bcdc5d57cdc9dd3cff5a597d4cc7
Dinika03 <78576083+Dinika03@users.noreply.github.com>
2025-08-12 16:39:20 -0400

    Update datalayerEDDL.js

--- datalayerEDDL.js
+++ datalayerEDDL.js
@@ -1,6 +1,3 @@
 window.EDDLdataLayer = window.EDDLdataLayer || [];
 
-function pushEDDLEvent(eventObj) {
-  window.EDDLdataLayer.events.push(eventObj);
-  console.log("EDDLdataLayer event:", eventObj); // Debug log
-}
+
