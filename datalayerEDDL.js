85ad8b01fe61a354f0347506769a41d3f32171a6
Dinika03 <78576083+Dinika03@users.noreply.github.com>
2025-10-14 22:45:55 -0400

    Update datalayerEDDL.js

--- datalayerEDDL.js
+++ datalayerEDDL.js
@@ -38,4 +38,24 @@ function updatebuttonclick(id){
     }
 
 }
+function updateFormSubmissionEvent(id){
+    console.log(id);
+     window.EDDLdataLayer.push({
+          event: "email_submit",
+          eventInfo: {
+            eventName: "email submit - get your quote",
+            eventAction: "email submitted",
+            eventType: "email"
+          }
+   
+    });
+    window.EDDLdataLayer.push({
+          event: "form_complete",
+          eventInfo: {
+            eventName: "form completed - get your quote",
+            eventAction: "form completed",
+            eventType: "form"
+          }
+        });
+}
 
